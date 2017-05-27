9d3148096e3105c87ddc84f4aff17d077a5e7747
Tomos-Evans <TomosAMEvans@gmail.com>
2017-05-27 09:14:32 +0100

    Changed to use double quotes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var main = function() {
 
-    $('form').submit(function(event) {
-        var $input = $(event.target).find('input');
+    $("form").submit(function(event) {
+        var $input = $(event.target).find("input");
         var comment = $input.val();
 
         if (comment !== "") {
-            var html = $('<li>').text(comment);
-            html.prependTo('#comments');
+            var html = $("<li>").text(comment);
+            html.prependTo("#comments");
             $input.val("");
         }
 
         return false;
     });
 
-}
+};
 
 $(document).ready(main);
